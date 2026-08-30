a684e27cd828483248b48c2baf1059a9048e7562
Shaik Nagoor Saheb <159810897+NagoorSaheb178@users.noreply.github.com>
2026-08-30 09:46:29 +0530

    Add className to ExtractingScreen component

--- src/components/ExtractingScreen.tsx
+++ src/components/ExtractingScreen.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 export default function ExtractingScreen() {
   return (
     <div
+      className="mobile-extracting-inner"
       style={{
         display: "flex",
         flexDirection: "column",
